813cb2a1117def524c4820544693d10d996af7c6
BuildTools <unconfigured@null.spigotmc.org>
2023-01-04 16:37:09 +0800

    proper quitting

--- App/main.py
+++ App/main.py
@@ -1,7 +1,9 @@
 from sys import exit as sysExit
-from threading import Thread
+from multiprocessing import Process
+from typing import Callable
 
 from Config import Config, ConfigInstance, setConfigPath
+from Singleton import isFirstInstance
 from WebApp import App
 
 configTemplate: Config = {
@@ -9,25 +11,44 @@ configTemplate: Config = {
     "webAppList": (["example"], "List of names of web apps"),
 }
 
+keywordMapping: dict[str, Callable] = {
+    "quit": sysExit,
+    "reload": lambda: None,
+    "restart": lambda: None,
+}
+
+rootName = "webServer"
+
+
+def singletonCallback(string: str) -> None:
+    print("Received message: " + string)
+
 
 def main():
-    setConfigPath("~/." + "webServer")
+    setConfigPath("~/." + rootName)
     config = ConfigInstance("main", configTemplate)
+    if isFirstInstance(rootName):
+        # start
+        appList: dict[str, App] = {}
+        threadList: dict[str, Process] = {}
+        for name in config["webAppList"]:
+            print("Initializing web app: " + name)
+            appList[name] = App(name)
+            threadList[name] = Process(target=appList[name].run)
+            threadList[name].start()
+            print("Web app: " + name + " initialized.")
 
-    appList: dict[str, App] = {}
-    threadList: dict[str, Thread] = {}
-    for name in config["webAppList"]:
-        print("Initializing web app: " + name)
-        appList[name] = App(name)
-        threadList[name] = Thread(target=appList[name].run)
-        threadList[name].start()
-        print("Web app: " + name + " initialized.")
-
-    while True:
         try:
-            input()
+            while True:
+                pass
         except KeyboardInterrupt:
+            for name, item in threadList.items():
+                item.terminate()
             sysExit(0)
+    else:
+        # quit for now
+        print("Another instance is running.")
+        sysExit(0)
 
 
 if __name__ == "__main__":
